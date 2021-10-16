dfc18a3eac874e90fb06659e407f70e809c4588b
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-10-16 21:13:59 +0600

    final version

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import Header from './components/Header';
 import Balance from './components/Balance'
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
+
+import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
+    <GlobalProvider>
       <Header />
       <div className = "container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
 export default App;
+
